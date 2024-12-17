e64cd4620dbeeee8bfac2fb94a8d40d483bd5519
Malik Hamza <malikhamza.v@gmail.com>
2024-12-17 14:11:43 +0500

    remove banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,16 @@
 <script setup>
-import Banner from '@/components/commonComponents/banner/Banner.vue';
-import Footer from '@/components/commonComponents/footer/Footer.vue';
-import Navbar from '@/components/commonComponents/navbar/Navbar.vue';
-import GuestOfHonour from '@/components/viewComponents/home/GuestOfHonour.vue';
-import PeopleBehindPeople from '@/components/viewComponents/home/PeopleBehindPeople.vue';
-import SecureYourSpot from '@/components/viewComponents/home/SecureYourSpot.vue';
-
+import Banner from "@/components/commonComponents/banner/Banner.vue";
+import Footer from "@/components/commonComponents/footer/Footer.vue";
+import Navbar from "@/components/commonComponents/navbar/Navbar.vue";
+import GuestOfHonour from "@/components/viewComponents/home/GuestOfHonour.vue";
+import PeopleBehindPeople from "@/components/viewComponents/home/PeopleBehindPeople.vue";
+import SecureYourSpot from "@/components/viewComponents/home/SecureYourSpot.vue";
 </script>
 
 <template>
-  
- 
-   <Banner/>
-   <PeopleBehindPeople/>
-   <GuestOfHonour/>
-   <SecureYourSpot/>
-   <Footer/>
+  <!-- <Banner/> -->
+  <PeopleBehindPeople />
+  <GuestOfHonour />
+  <SecureYourSpot />
+  <Footer />
 </template>
